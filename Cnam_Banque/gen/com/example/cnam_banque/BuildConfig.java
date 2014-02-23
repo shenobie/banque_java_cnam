0601cf19602b06b497d985cf34f47de51f9dbbd0
Grégoire <julien_78400@hotmail.fr>
2014-02-23 17:21:59 +0100

    ajout du workspace de l'appli android

---
+++ Cnam_Banque/gen/com/example/cnam_banque/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.cnam_banque;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Cnam_Banque/src/com/example/cnam_banque/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.cnam_banque;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity
+{
+
+	
+
+}
