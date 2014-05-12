8eb14abd1deb2b7b86df5517a3c20f86ec8b423e
projetgl <mzehamid@gmail.com>
2014-05-13 01:19:07 +0200

    Merge branch 'master' of https://github.com/shenobie/banque_java_cnam
    Conflicts:
    	AndroCnam/bin/AndroCnam.apk
    	AndroCnam/bin/classes.dex
    	AndroCnam/bin/resources.ap_

--- AndroCnam/gen/com/example/androcnam/R.java
+++ AndroCnam/gen/com/example/androcnam/R.java
@@ -22,35 +22,61 @@ public final class R {
     }
     public static final class drawable {
         public static final int cnam=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int coffrefort=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int rib=0x7f020003;
+        public static final int virement=0x7f020004;
     }
     public static final class id {
-        public static final int BoutonConnection=0x7f080005;
-        public static final int action_settings=0x7f080006;
-        public static final int editLogin=0x7f080001;
-        public static final int editPassword=0x7f080002;
-        public static final int textView1=0x7f080000;
-        public static final int textView2=0x7f080004;
-        public static final int textView3=0x7f080003;
+        public static final int BoutonConnection=0x7f080003;
+        public static final int accueilsommaire=0x7f080009;
+        public static final int action_settings=0x7f08000e;
+        public static final int afficherib=0x7f080008;
+        public static final int editLogin=0x7f080002;
+        public static final int editPassword=0x7f080000;
+        public static final int imageBoutonComptes=0x7f08000b;
+        public static final int imageBoutonRib=0x7f08000d;
+        public static final int imageBoutonVirement=0x7f08000c;
+        public static final int imageView1=0x7f080004;
+        public static final int mescomptes=0x7f080007;
+        public static final int textView1=0x7f080006;
+        public static final int textView2=0x7f080001;
+        public static final int textView3=0x7f080005;
+        public static final int virements=0x7f08000a;
     }
     public static final class layout {
-        public static final int activity_menu_principal=0x7f030000;
-        public static final int activity_sommaire=0x7f030001;
+        public static final int activity_afficher_rib=0x7f030000;
+        public static final int activity_effectuer_virement=0x7f030001;
+        public static final int activity_menu_principal=0x7f030002;
+        public static final int activity_selection_comptes=0x7f030003;
+        public static final int activity_sommaire=0x7f030004;
     }
     public static final class menu {
-        public static final int menu2=0x7f070000;
-        public static final int menu_principal=0x7f070001;
-        public static final int sommaire=0x7f070002;
+        public static final int afficher_rib=0x7f070000;
+        public static final int effectuer_virement=0x7f070001;
+        public static final int menu2=0x7f070002;
+        public static final int menu_principal=0x7f070003;
+        public static final int selection_comptes=0x7f070004;
+        public static final int sommaire=0x7f070005;
     }
     public static final class string {
+        public static final int accueilRib=0x7f050010;
+        public static final int accueilVirement=0x7f05000e;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int bonjour=0x7f050002;
         public static final int connection=0x7f050005;
-        public static final int hello_world=0x7f050007;
         public static final int login=0x7f050003;
         public static final int mdp=0x7f050004;
+        public static final int mescomptes=0x7f050008;
+        public static final int rib=0x7f05000a;
+        public static final int selectionComptes=0x7f05000c;
+        public static final int sommaire=0x7f050007;
+        public static final int title_activity_afficher_rib=0x7f05000f;
+        public static final int title_activity_effectuer_virement=0x7f05000d;
+        public static final int title_activity_selection_comptes=0x7f05000b;
         public static final int title_activity_sommaire=0x7f050006;
+        public static final int virement=0x7f050009;
     }
     public static final class style {
         /** 
