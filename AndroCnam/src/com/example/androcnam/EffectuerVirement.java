4e8ba6cd624ab25caed96fda84492aeac41c9eb2
projetgl <mzehamid@gmail.com>
2014-05-19 04:00:55 +0200

    Virement ON
    EZPZ

--- AndroCnam/src/com/example/androcnam/EffectuerVirement.java
+++ AndroCnam/src/com/example/androcnam/EffectuerVirement.java
@@ -1,16 +1,137 @@
 package com.example.androcnam;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
-import android.view.Menu;
-
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+ 
 public class EffectuerVirement extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_effectuer_virement);
-	}
 	
-
+	
+	// url du fichier php réalisant le virement
+    private static final String url = "http://192.168.1.38/banque/virement.php";
+	 
+    // Progress Dialog
+    private ProgressDialog pDialog;
+ 
+    JSONParser jsonParser = new JSONParser();
+    EditText inputId;
+    EditText inputMontant;
+    EditText inputLibelle;
+    String pid;
+ 
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_effectuer_virement);
+        
+     // on récupère la variable transmise par l'intent précédent
+     		Intent intent = getIntent();
+     		pid = intent.getStringExtra("id");
+ 
+        // Edition de textes
+        inputId = (EditText) findViewById(R.id.inputId);
+        inputMontant = (EditText) findViewById(R.id.inputMontant);
+        inputLibelle = (EditText) findViewById(R.id.inputLibelle);
+ 
+        // Création du bouton
+        Button btnCreateProduct = (Button) findViewById(R.id.btnCreateProduct);
+ 
+        // listener sur le bouton
+        btnCreateProduct.setOnClickListener(new View.OnClickListener() {
+ 
+            @Override
+            public void onClick(View view) {
+                // creating new product in background thread
+                new CreateNewProduct().execute();
+            }
+        });
+    }
+ 
+    /**
+     * Background Async Task to Create new product
+     * */
+    class CreateNewProduct extends AsyncTask<String, String, String> {
+ 
+        /**
+         * Boite de dialogue
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(EffectuerVirement.this);
+            pDialog.setMessage("Virement en cours...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+ 
+        /**
+         * Virement
+         * */
+        protected String doInBackground(String... args) {
+            String cible = inputId.getText().toString();
+            String montant = inputMontant.getText().toString();
+            String libelle = inputLibelle.getText().toString();
+ 
+            // creation des paramètres
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("cible", cible));
+            params.add(new BasicNameValuePair("montant", montant));
+            params.add(new BasicNameValuePair("libelle", libelle));
+ 
+            
+            // Creation de l'url grace à l'objeret JSON et la methode GET
+            JSONObject json = jsonParser.makeHttpRequest(url,
+                    "GET", params);
+ 
+            // check log cat fro response
+            Log.d("Create Response", json.toString());
+ 
+            // check for success tag
+            try {
+                int success = json.getInt(TAG_SUCCESS);
+ 
+                if (success == 1) {
+                    // Virement effectué avec succès
+                    Intent i = new Intent(getApplicationContext(), EffectuerVirement.class);
+                    startActivity(i);
+ 
+                    // actualise l'ecran
+                    finish();
+                } else {
+                    // echec du virement
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+ 
+            return null;
+        }
+ 
+        /**
+         * Après la fin de tâche en arrière plan
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once done
+            pDialog.dismiss();
+        }
+ 
+    }
 }
